0daf8eed45d7610caad07235a1e61d564bd679d6
cocoa-1012 <cocoa.pandaok@gmail.com>
2022-03-16 05:46:06 -0400

    init

--- src/App.js
+++ src/App.js
@@ -1,28 +1,7 @@
 import "./App.css";
-import backgroundImg from "./assets/images/background.jpg";
-import { Header } from "./components/Header";
 
 function App() {
-  return (
-    <div className="App">
-      <Header />
-      <img className="w-screen h-screen" src={backgroundImg} alt="background" />
-      {/* <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p className="p-5">
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link text-2xl font-bold underline"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header> */}
-    </div>
-  );
+  return <div className="App bg-main-background min-h-screen bg-cover"></div>;
 }
 
 export default App;
