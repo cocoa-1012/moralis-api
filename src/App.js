246c40dbb72a7b26f1181de04d87b29e9ef3bf02
cocoa-1012 <cocoa.pandaok@gmail.com>
2022-03-17 12:20:29 -0400

    update display filtered data

--- src/App.js
+++ src/App.js
@@ -1,13 +1,23 @@
 import { useMoralis } from "react-moralis";
 import "./App.css";
+import { DisplayComponent } from "./components/DisplayComponent";
 import { Header } from "./components/Header";
 import { InputComponent } from "./components/InputComponent";
 
 function App() {
-  const { authenticate, isAuthenticated, user } = useMoralis();
+  const {
+    Moralis,
+    logout,
+    enableWeb3,
+    isInitialized,
+    isWeb3Enabled,
+    authenticate,
+    isAuthenticated,
+    user,
+  } = useMoralis();
 
   // if (!isAuthenticated) {
-  if (!isAuthenticated) {
+  if (isAuthenticated) {
     return (
       <div>
         <button onClick={() => authenticate()}>Authenticate</button>
@@ -17,7 +27,10 @@ function App() {
   return (
     <div className="App bg-main-background min-h-screen bg-cover">
       <Header />
+
+      {/* <h1>Welcome {user.get("username")}</h1> */}
       <InputComponent />
+      <DisplayComponent />
       {/* <h1>Welcome {user.get("username")}</h1> */}
     </div>
   );
---
+++ src/components/DisplayComponent.js
@@ -0,0 +1,9 @@
+export const DisplayComponent = () => {
+  return (
+    <div className="flex justify-center w-full items-center py-16">
+      <p className="text-white text-5xl font-semibold ">
+        Display Filtered Component
+      </p>
+    </div>
+  );
+};
