7399ebcc94b80225d2ce7d9773b5542d25f0b92e
nanth1012 <babypigdev@gmail.com>
2022-03-31 08:03:34 -0400

    update getting metadata

--- src/components/DisplayComponent.js
+++ src/components/DisplayComponent.js
@@ -2,10 +2,6 @@ import Moralis from "moralis/dist/moralis.min.js";
 import React, { useEffect, useState } from "react";
 import { useMoralisWeb3Api } from "react-moralis";
 
-// const appId = (“xxxx”);
-// const serverUrl = “https://xxx”;
-// Moralis.start({ serverUrl , appId })
-
 export const DisplayComponent = () => {
   const [collectionAddress, setCollectionAddress] = useState(
     "0x608c2feb6b80993b26ffb6fa84f454ad3ac38bf0"
@@ -18,7 +14,12 @@ export const DisplayComponent = () => {
   const [traits, setTraits] = useState();
   const [lowestTraitValues, setLowestTraitValues] = useState();
   const [selectedTraitType, setSelectedTraitType] = useState("Select");
+  const [selectedTraitTypeForSort, setSelectedTraitTypeForSort] =
+    useState("Select");
   const [selectedTrait, setSelectedTrait] = useState("Select");
+  const [traitSortData, setTraitSortData] = useState([]);
+  const [traitRankData, setTraitRankData] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
 
   const serverUrl = "https://oop2hhtnogj0.usemoralis.com:2053/server";
   const appId = "RUKN49vcmeNc0vHrSNyl5Z3MWvY0ILvd2uiBOxeE";
@@ -29,13 +30,10 @@ export const DisplayComponent = () => {
     if (!currentUser) {
       currentUser = await Moralis.Web3.authenticate();
     }
-    // console.log("Current user moralis", currentUser);
-    // const userEthNFT = await Moralis.Web3.getNFTs();
-    // console.log("userEthNFT==? ", userEthNFT);
-    //Get metadata for one token. Ex: USDT token on ETH
+
     const options = {
       chain: "eth",
-      addresses: "0xdAC17F958D2ee523a2206206994597C13D831ec7",
+      addresses: "0x9712228cEeDA1E2dDdE52Cd5100B88986d1Cb49c",
     };
     const tokenMetadata = await Web3Api.token.getTokenMetadata(options);
     console.log("Dan-tokenMetadata==>", tokenMetadata);
@@ -44,11 +42,11 @@ export const DisplayComponent = () => {
   const setDefault = () => {
     setCollectionAddress("0x608c2feb6b80993b26ffb6fa84f454ad3ac38bf0");
   };
+
   // Calculate size with price
   const compare = (a, b) => {
     return a.price < b.price ? 1 : a.price > b.price ? -1 : 0;
   };
-
   // Get nft prices
   const fetchNFTTrades = async () => {
     const options = {
@@ -62,13 +60,7 @@ export const DisplayComponent = () => {
     const newArray = NFTTrades.result.sort(compare);
     setLowestValues(newArray.slice(-2));
   };
-  //Get Matadata directly
-  const fetchMetaDataDirectly = async () => {
-    initializeApp();
 
-    const userEthNFT = await Moralis.Web3.getNFTs();
-    console.log("userEthNFT==? ", userEthNFT);
-  };
   // Get Trait List
   const fetchAllTokenIds = async () => {
     let trait_list = [];
@@ -97,7 +89,6 @@ export const DisplayComponent = () => {
     traitNFTs &&
       traitNFTs.result.map((item) => {
         if (typeof JSON.parse(item.metadata) === "object") {
-          console.log("OK, this is object", JSON.parse(item.metadata));
           JSON.parse(item.metadata) &&
             JSON.parse(item.metadata).attributes.map((option) => {
               if (option.trait_type === selectedTraitType) {
@@ -115,8 +106,6 @@ export const DisplayComponent = () => {
   };
   // Get Lowest NFT by trait
   const fetchLowestByTrait = () => {
-    console.log("traitResult", traitResult);
-    console.log("selectedTrait", selectedTrait);
     let result = [];
     totalNFTTrades &&
       traitResult &&
@@ -130,8 +119,6 @@ export const DisplayComponent = () => {
 
     const newArray = result.sort(compare);
     setLowestTraitValues(newArray.length > 2 ? newArray.slice(-2) : newArray);
-    console.log("lowestTraitValues", lowestTraitValues);
-    console.log("newArray", newArray);
   };
 
   //Click Start button
@@ -139,15 +126,77 @@ export const DisplayComponent = () => {
     fetchNFTTrades();
     fetchAllTokenIds();
     initializeApp();
-    // fetchMetaDataDirectly();
-    // console.log("This is starting button ===>");
+
+    getTraitRanking();
+  };
+
+  const traitCompare = (a, b) => (a.val > b.val ? -1 : 1);
+
+  const parseSnapShot = (result, array) => {
+    array.map((element) => {
+      if (element.metadata) {
+        const json = JSON.parse(element.metadata);
+        const attributes = json.attributes;
+        const len = attributes.length;
+        for (let i = 0; i < len; i++) {
+          const key = attributes[i].trait_type;
+          const val = attributes[i].value;
+          const trait = result[key] ? result[key] : [];
+          const index = trait.findIndex((element) => element.key === val);
+          if (index === -1) {
+            trait.push({ key: val, val: 1 });
+          } else {
+            trait[index].val += 1;
+          }
+          result[key] = trait;
+        }
+      }
+    });
+  };
+
+  const getTraitRanking = async () => {
+    let result = {};
+    const options = {
+      chain: "eth",
+      address: collectionAddress,
+    };
+    let snapShot = await Web3Api.token.getAllTokenIds(options);
+    const total = snapShot.total;
+    parseSnapShot(result, snapShot.result);
+    for (let i = 500; i < total; i += 500) {
+      snapShot = await Web3Api.token.getAllTokenIds({
+        ...options,
+        offest: i,
+      });
+      parseSnapShot(result, snapShot.result);
+    }
+    const keys = Object.keys(result);
+    const len = keys.length;
+    for (let i = 0; i < len; i++) {
+      const array = result[keys[i]];
+      array.sort(traitCompare);
+      result[keys[i]] = array;
+    }
+    console.log(result);
+    console.log(Object.keys(result));
+    setTraitSortData(result);
+    setIsLoading(false);
+  };
+
+  const handleSortedTrait = (e) => {
+    setSelectedTraitTypeForSort(e);
+    setTraitRankData(traitSortData[e]);
+    console.log("SetTraitRandData", e, traitSortData[e]);
   };
 
   useEffect(() => {
     totalNFTTrades && fetchTypesofTrait();
     selectedTrait && fetchLowestByTrait();
-    // console.log("Selected option", selectedTraitType);
-  }, [selectedTraitType, selectedTrait]);
+  }, [selectedTraitType, selectedTraitTypeForSort, selectedTrait]);
+
+  useEffect(() => {
+    console.log("selectedTraitTypeForSort:", selectedTraitTypeForSort);
+  }, [selectedTraitTypeForSort]);
   return (
     <>
       <div className="flex justify-center w-full items-center py-0">
@@ -175,7 +224,7 @@ export const DisplayComponent = () => {
       </div>
       {/* Getting floor price of last two least values on open sea */}
       <div className="text-white text-3xl font-semibold ">
-        - Getting floor price of last two least values on Opensea
+        - Floor price of last two least values on Opensea
       </div>
       {lowestValues && (
         <div className="lowest_group flex justify-center p-4">
@@ -192,7 +241,7 @@ export const DisplayComponent = () => {
       )}
       {/* Getting floor price of last two least values by trait on open sea */}
       <div className="text-white text-3xl font-semibold ">
-        - Getting floor price of last two least values by trait on Opensea
+        - Floor price of last two least values by trait on Opensea
       </div>
       <div className="flex justify-center">
         {traitTypes && (
@@ -260,24 +309,50 @@ export const DisplayComponent = () => {
       <div className="text-white text-3xl font-semibold ">
         - Getting metadata from the ethereum blockchainby trait ranking
       </div>
-      <div className="text-white text-xl font-semibold flex px-[10%]">
-        <table className="w-screen bg-green-300">
-          <tr className="text-black">
-            <th>ID</th>
-            <th>Explore</th>
-            <th>Score</th>
-            <th>Rank</th>
-            <th>Price</th>
-          </tr>
-          <tr className="text-gray-700">
-            <td>id</td>
-            <td>explore</td>
-            <td>score</td>
-            <td>rank</td>
-            <td>price</td>
-          </tr>
-        </table>
-      </div>
+      {traitSortData.length !== 0 ? (
+        <div className="text-white text-xl font-semibold flex px-[10%]">
+          {traitTypes && (
+            <div className="text-black lowest_group flex justify-center p-4">
+              <select
+                className=""
+                value={selectedTraitTypeForSort}
+                onChange={(e) => handleSortedTrait(e.target.value)}
+              >
+                <option value="Select" disabled>
+                  Select...
+                </option>
+                {traitTypes.map((item, i) => (
+                  <option className="" id={item} value={item}>
+                    {item}
+                  </option>
+                ))}
+              </select>
+            </div>
+          )}
+          {traitRankData !== [] && (
+            <table className="w-screen bg-green-300">
+              <tbody>
+                <tr className="text-black">
+                  <th>Trait</th>
+                  <th>Score</th>
+                  <th>Rank</th>
+                  <th>Price</th>
+                </tr>
+                {traitRankData.map((item, i) => (
+                  <tr className="text-gray-700">
+                    <td>{item.key}</td>
+                    <td>{item.val}</td>
+                    <td>{i + 1}</td>
+                    <td>price</td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+          )}
+        </div>
+      ) : (
+        <div>Loading...</div>
+      )}
     </>
   );
 };
